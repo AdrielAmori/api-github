ecff9c4503efe0af628e238be5546cfbe7216e6f
Adriel Arruda Amorim <adriel680@gmail.com>
2024-07-06 14:48:09 -0300

    informações de repositórios

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -6,18 +6,25 @@ import Repositories from './Repositories';
 
 import { Loading, Container, Sidebar, Main } from './styles';
 
-import { getUser, getLangsFrom } from '../../services/api';
+import { getUser, getRepos, getLangsFrom } from '../../services/api';
 
 const RepositoriesPage = () => {
   const [user, setUser] = useState();
+  const [repositories, setRepositories] = useState();
+  const [languages, setLanguages] = useState();
   const [currentLanguage, setCurrentLanguage] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadData = async () => {
-      const [userResponse] = await Promise.all([getUser('AdrielAmori')]);
+      const [userResponse, repositoriesResponse] = await Promise.all([
+        getUser('AdrielAmori'),
+        getRepos('AdrielAmori'),
+      ]);
 
       setUser(userResponse.data);
+      setRepositories(repositoriesResponse.data);
+      setLanguages(getLangsFrom(repositoriesResponse.data));
 
       setLoading(false);
     };
@@ -25,70 +32,12 @@ const RepositoriesPage = () => {
     loadData();
   }, []);
 
-  // const user = {
-  //   login: 'AdrielAmori',
-  //   name: 'Adriel Arruda Amorim',
-  //   avatar_url: 'https://avatars.githubusercontent.com/u/144482242?v=4',
-  //   followers: 0,
-  //   following: 0,
-  //   company: null,
-  //   blog: 'https://github.com/adrielamori',
-  //   location: 'Fortaleza',
-  // };
-
-  const repositories = [
-    {
-      id: '1',
-      name: 'Repo 1',
-      description: 'Descrição',
-      html_url: 'https://github.com',
-      language: 'Javascript',
-    },
-    {
-      id: '2',
-      name: 'Repo 2',
-      description: 'Descrição',
-      html_url: 'https://github.com',
-      language: 'Python',
-    },
-    {
-      id: '3',
-      name: 'Repo 3',
-      description: 'Descrição',
-      html_url: 'https://github.com',
-      language: 'Javascript',
-    },
-    {
-      id: '4',
-      name: 'Repo 4',
-      description: 'Descrição',
-      html_url: 'https://github.com',
-      language: 'PHP',
-    },
-    {
-      id: '5',
-      name: 'Repo 5',
-      description: 'Descrição',
-      html_url: 'https://github.com',
-      language: 'Ruby',
-    },
-    {
-      id: '6',
-      name: 'Repo 6',
-      description: 'Descrição',
-      html_url: 'https://github.com',
-      language: 'TypeScript',
-    },
-  ];
-
-  const languages = getLangsFrom(repositories);
-
   const onFilterClick = (language) => {
     setCurrentLanguage(language);
   };
 
-  if(loading){
-    return <Loading> Carregando... </Loading>
+  if (loading) {
+    return <Loading> Carregando... </Loading>;
   }
 
   return (
